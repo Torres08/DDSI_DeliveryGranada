0cf457daa79116674df983b3a0b0b38e69820d71
Torres <51020979+Torres08@users.noreply.github.com>
2023-11-19 23:18:47 +0100

    cambios pero sigue sin funcionar, usar como referencia

--- database_connection.py
+++ database_connection.py
@@ -8,6 +8,7 @@
 import pyodbc
 
 # Configuración de la conexión
+driver = 'ODBC Driver 18 for SQL Server'  # Asegúrate de usar el nombre correcto del driver
 host = 'oracle0.ugr.es'
 port = 1521
 database = 'practbd.oracle0.ugr.es'
@@ -15,8 +16,15 @@ user = 'x0596044'
 password = 'x0596044x'
 
 # Cadena de conexión
-cadena_conexion = f'SERVER={host},{port};DATABASE={database};UID={user};PWD={password}'
-# Intentar realizar la conexión
+cadena_conexion = (
+    f'DRIVER={driver};'
+    f'SERVER={host},{port};'
+    f'DATABASE={database};'
+    f'UID={user};'
+    f'PWD={password};'
+    'Timeout=600;'  # Ajusta el valor de Timeout según tus necesidades
+)
+
 try:
     # Conectar a la base de datos
     connection = pyodbc.connect(cadena_conexion)
@@ -25,11 +33,8 @@ try:
     # Crear un cursor para ejecutar consultas SQL
     cursor = connection.cursor()
     
+    # Aquí puedes agregar tus sentencias SQL
     
-    # SENTENCIAS SQL QUE QUERAMOS AÑADIR
-
-
-
 except pyodbc.Error as e:
     print(f"Error de base de datos: {e}")
 finally:
@@ -39,6 +44,5 @@ finally:
     if 'connection' in locals():
         connection.close()
         print("Conexión cerrada.")
-
 
 
---
+++ settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'practbd.oracle0.ugr.es',
+        'USER': 'x0596044',
+        'PASSWORD': 'x0596044x',
+        'HOST': 'oracle0.ugr.es',
+        'PORT': '1521',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 18 for SQL Server',
+        },
+    }
+}
