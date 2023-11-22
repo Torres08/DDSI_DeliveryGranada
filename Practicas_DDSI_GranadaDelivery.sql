2b364f7be394945ede86a79a8f35f42f0f18a0a1
Torres <51020979+Torres08@users.noreply.github.com>
2023-11-22 17:29:42 +0100

    seminario 1 set up
    para empezar con el seminario 1

--- Practicas_DDSI_GranadaDelivery.sql
+++ Practicas_DDSI_GranadaDelivery.sql
@@ -1,2 +1,34 @@
-describe CLIENTE;
-select * from cliente;
+-- caso ejemplo de 
+
+-- Crear la tabla Producto
+CREATE TABLE Producto (
+    IDProducto INT PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL,
+    Descripcion TEXT,
+    Precio DECIMAL(10, 2) NOT NULL
+);
+
+-- Crear la tabla Stock
+CREATE TABLE Stock (
+    IDProducto INT PRIMARY KEY,
+    Cantidad INT NOT NULL,
+    FOREIGN KEY (IDProducto) REFERENCES Producto(IDProducto)
+);
+
+-- Crear la tabla Pedido
+CREATE TABLE Pedido (
+    Cpedido INT PRIMARY KEY,
+    Ccliente INT NOT NULL,
+    FechaPedido DATE NOT NULL,
+    FOREIGN KEY (Ccliente) REFERENCES Cliente(Ccliente)
+);
+
+-- Crear la tabla DetallePedido
+CREATE TABLE DetallePedido (
+    Cpedido INT,
+    IDProducto INT,
+    Cantidad INT NOT NULL,
+    PRIMARY KEY (Cpedido, IDProducto),
+    FOREIGN KEY (Cpedido) REFERENCES Pedido(Cpedido),
+    FOREIGN KEY (IDProducto) RE
+);
--- database_connection.py
+++ database_connection.py
@@ -1,48 +1,66 @@
-# Conexion a la base de datos
-# Autor: Torres Ramos, Juan Luis
-# Grupo 8 DeliveryGranada
 
-# Paquetes usados: 
-# pyodbc: pip install  pyodbc
+#!usr/bin/python
 
+import pymysql
 import pyodbc
 
-# Configuración de la conexión
-driver = 'ODBC Driver 18 for SQL Server'  # Asegúrate de usar el nombre correcto del driver
-host = 'oracle0.ugr.es'
-port = 1521
-database = 'practbd.oracle0.ugr.es'
-user = 'x0596044'
-password = 'x0596044x'
+class BaseDatos:
 
-# Cadena de conexión
-cadena_conexion = (
-    f'DRIVER={driver};'
-    f'SERVER={host},{port};'
-    f'DATABASE={database};'
-    f'UID={user};'
-    f'PWD={password};'
-    'Timeout=600;'  # Ajusta el valor de Timeout según tus necesidades
-)
+    Cpedido = 0
 
-try:
-    # Conectar a la base de datos
-    connection = pyodbc.connect(cadena_conexion)
-    print("Conexión exitosa.")
+    def __init__(self):  # Conecta con la base de datos
+        # Variable que comprueba si están creadas las tablas o no
+        self.tablas_creadas = False
+        self.tablas_borradas = False  # True
+        
+        driver = 'ODBC Driver 18 for SQL Server'  # Asegúrate de usar el nombre correcto del driver
+        host = 'oracle0.ugr.es'
+        port = 1521
+        database = 'practbd.oracle0.ugr.es'
+        user = 'x0596044'
+        password = 'x0596044'
 
-    # Crear un cursor para ejecutar consultas SQL
-    cursor = connection.cursor()
-    
-    # Aquí puedes agregar tus sentencias SQL
-    
-except pyodbc.Error as e:
-    print(f"Error de base de datos: {e}")
-finally:
-    # Cerrar el cursor y la conexión al finalizar
-    if 'cursor' in locals():
-        cursor.close()
-    if 'connection' in locals():
-        connection.close()
+        # Cadena de conexión
+        cadena_conexion = (
+            f'DRIVER={driver};'
+            f'SERVER={host},{port};'
+            f'DATABASE={database};'
+            f'UID={user};'
+            f'PWD={password};'
+        )
+
+        try:
+            # CONECTO CON LA BASE DE DATOS UGR
+            self.cnxn = pyodbc.connect(cadena_conexion)
+            # self.cnxn = pymysql.connect(host="127.0.0.1", user="root", password="1234567890", database="DDSI")
+            print("CONEXION BD OK")
+
+            # Preparo el cursor para poder acceder a las tablas
+            self.cursor = self.cnxn.cursor()
+
+        except pymysql.Error as e:
+            print("OCURRIO UN ERROR AL CONECTAR: ", e)
+
+    def verificar_conexion(self):
+        if hasattr(self, 'cnxn') and self.cnxn.is_connected():
+            print("La conexión está activa.")
+        else:
+            print("La conexión no está activa.")
+
+def main():
+    # Crear una instancia de la clase BaseDatos
+    bd = BaseDatos()
+
+    # Verificar la conexión
+    bd.verificar_conexion()
+
+    # Cerrar la conexión
+    if hasattr(bd, 'cnxn') and bd.cnxn.is_connected():
+        bd.cnxn.close()
         print("Conexión cerrada.")
+
+if __name__ == "__main__":
+    main()
+
 
 
